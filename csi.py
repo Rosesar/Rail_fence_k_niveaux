f2b04745b231d25d0c2fb30b84dbb694cde65c6d
Rosesar <162746376+Rosesar@users.noreply.github.com>
2024-12-15 18:28:20 +0100

    csi.py

--- csi.py
+++ csi.py
@@ -99,7 +99,7 @@ else:
     # Fallback to manual input
     text = st.text_input("Entrer le texte:", " ")
 # Input for custom filename
-file_name = st.text_input("Nom du fichier de sortie (sans extension):", value="output_file")
+file_name = st.text_input("Nom du fichier de sortie (sans extension):", value="fichier")
 
 if st.button("Appliquer l'opération"):
     if not file_name.strip():
